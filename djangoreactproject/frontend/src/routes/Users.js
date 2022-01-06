889fb50016be7cef214aeb991fe2327ad9ad2d8f
Alexsik76 <alex@jurist.vn.ua>
2022-01-06 15:16:08 +0200

    reworked Users.js

--- djangoreactproject/frontend/src/routes/Users.js
+++ djangoreactproject/frontend/src/routes/Users.js
@@ -3,62 +3,82 @@ import TableRow from '../components/TableRow'
 import axios from "axios";
 // import {Link} from "react-router-dom";
 // import ButtonDelete from "../components/ButtonDelete";
-
-
-
+import Loader from "react-loader-spinner";
 
 export default function UsersList() {
     const [users, setData] = useState([]);
-
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
-        async function fetchData () {
+        setIsLoading(true)
+        setTimeout(fetchData, 2000)
+        setIsLoading(false)
+        },[]);
+
+
+    async function fetchData() {
         try {
-            const result = await axios.get('http://localhost:8000/api/users');
+            let response = await axios.get('http://localhost:8000/api/users')
+            response = await response.data
+            setData(response)
 
-            setData(result.data);
+        } catch (error) {
+            console.error(error);
         }
-        catch (error) {
-    console.error(error);
-  }
     }
-        fetchData();
 
-    },[]);
-
+    async function deleteUser(pk) {
+        try {
+            await axios.delete(`http://localhost:8000/api/users/${pk}/`)
+            setData(users.filter((obj)=>{ return obj.pk !== pk})
+            )
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-
         <div className="users--list">
-            <table className="table table-hover">
-                <thead key="thead" className="thead-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Username</th>
-                    <th>Group</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {users.map(c =>
-                    <tr key={c.pk}>
-                        < TableRow user={c}
-                        />
-                        <td>
-                            {/* <Link to={`/users/${c.pk}`} className="btn btn-outline-success"> */}
-                            {/*    Update */}
-                            {/* </Link> */}
-                        </td>
-                        <td>
-                            {/* <ButtonDelete pk={c.pk} */}
-                            {/*              onUsersClick={this.onUsersClick} */}
-                            {/*              service={usersServices} */}
-                            {/* /> */}
-                        </td>
+            {(isLoading || users.length === 0) ?
+                <Loader type="Bars" color="#00BFFF" height={80} width={80} /> :
+                <table className="table table-hover">
+                    <thead key="thead" className="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Username</th>
+                        <th>Group</th>
+                        <th>Actions</th>
                     </tr>
-                )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {users.map(c =>
+                        <tr key={c.pk}>
+                            < TableRow user={c}
+                            />
+                            <td>
+                                {/* <Link to={`/users/${c.pk}`} className="btn btn-outline-success"> */}
+                                {/*    Update */}
+                                {/* </Link> */}
+                            </td>
+                            <td>
+                                <button onClick={() => {
+                                    deleteUser(c.pk)
+                                }
+                                }
+                                >
+                                    Delete user
+                                </button>
+                                {/* <ButtonDelete pk={c.pk} */}
+                                {/*              onUsersClick={this.onUsersClick} */}
+                                {/*              service={usersServices} */}
+                                {/* /> */}
+                            </td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            }
         </div>
 
     )
+
 }
