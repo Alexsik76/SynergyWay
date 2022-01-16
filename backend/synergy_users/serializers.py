a67b2cd8cf5da736ca1aa7da6547a40632dbb3cd
Alexsik76 <alex@jurist.vn.ua>
2022-01-16 07:51:47 +0200

    Starting Django 2

---
+++ backend/synergy_users/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from .models import SynergyGroup, SynergyUser
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SynergyGroup
+        fields = ('pk', 'name', 'description')
+   
+
+class GroupFieldSerializer(serializers.RelatedField):
+
+    queryset = SynergyGroup.objects.all()
+
+    def to_representation(self, value):
+        return value.name
+
+    def to_internal_value(self, data):
+        obj = self.queryset.get(name=data)
+        return obj
+
+
+class UserSerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(format="%H:%M:%S %d-%m-%Y", required=False)
+    group = GroupFieldSerializer()
+
+    class Meta:
+        model = SynergyUser
+        fields = ('pk', 'username', 'created', 'group')
+
